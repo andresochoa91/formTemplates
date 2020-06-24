a2d0fd58af9e3e8547c867d155e8ef309e1d20dc
andresochoa91 <andres_ochoa91@hotmail.com>
2020-06-23 20:17:07 -0700

    with some refactoring

--- app.js
+++ app.js
@@ -9,24 +9,32 @@ calendar.setAttribute('value', today);
 
 //To create people options
 (function () {
+  const select = document.querySelector("#people");
+  let options = "";
+  let option;
   for (let i = 1; i <= 20; i++) {
-    let option = document.createElement("option");
     if (i === 1) {
-      option.textContent = `${i} person`;
+      option = `<option value="${i}">${i} person</option>`;
     } else {
-      option.textContent = `${i} people`;
+      option = `<option value="${i}">${i} people</option>`;
     }
-    document.querySelector("#people").appendChild(option);
+    options += option;
   }
+  select.innerHTML = options;
 })();
 
+
 //To create schedule options
-(function () {
-  let hour = 10;
-  let ampm = "AM";
-  let half = "00"
-  
-  for (let i = 0; i < 29; i++) {
+createScheduleOptions('10:00 AM', '12:00 AM');
+function createScheduleOptions(openTime, closeTime) {
+  const numberOfTimeSlots = getNumberOfTimeSlots(openTime, closeTime);
+  const openTimeArray = openTime.split(':');
+  let hour = parseInt(openTimeArray[0]);
+  let ampm = openTime.includes("AM") ? "AM" : "PM";
+  let times = "";
+  let hours = document.querySelector("#hours");
+  let half = openTimeArray[1].includes('30') ? "30": "00";
+  for (let i = 0; i <= numberOfTimeSlots; i++) {
     if (hour === 12 && ampm === "AM") {
       ampm = "PM"
     } else if (hour === 13) {
@@ -34,14 +42,58 @@ calendar.setAttribute('value', today);
     } else if (i === 28) {
       ampm = "AM";
     }
-    let option = document.createElement("option");
-    option.textContent = `${hour}:${half} ${ampm}`;
-    document.querySelector("#hours").appendChild(option);
-    hour = i % 2 !== 0 ? hour + 1 : hour;
+    const option = `<option value="${hour}:${half}${ampm}">${hour}:${half} ${ampm}</option>`;
+    times += option;
+    hour = half === "00" ? hour : hour + 1;
     half = half === "00" ? "30" : "00";
   }  
-})();
-             
+  hours.innerHTML = times;
+}
+
+// times will look like 10:30 AM 10:00 PM 10.5 to 22
+function getNumberOfTimeSlots(openTime, closeTime) {
+  const openTimeNumeric = convertTimeStringToNumber(openTime);
+  const closeTimeNumeric = convertTimeStringToNumber(closeTime);
+  const reservationSlots = (closeTimeNumeric - openTimeNumeric) * 2;
+  return reservationSlots;
+}
+
+function convertTimeStringToNumber(time) {
+  const isTimeAM = time.includes('AM');
+  const isTimeTwelve = time.includes('12');
+  const timeSplit = time.split(':');
+  let timeNumeric;
+  if (timeSplit[1].includes('30')) {
+    timeNumeric = parseInt(timeSplit[0]) + 0.5;
+    if (shouldAdd12Hours(isTimeAM, isTimeTwelve)) {
+      timeNumeric += 12;
+    }
+  } else {
+     timeNumeric = parseInt(timeSplit[0]);
+     if (shouldAdd12Hours(isTimeAM, isTimeTwelve)) {
+      timeNumeric += 12;
+     }
+  }
+  return timeNumeric;
+}
+
+function shouldAdd12Hours(isTimeAM, isTimeTwelve) {
+  if (isTimeAM) {
+    if (isTimeTwelve) {
+      return true;
+    }
+  } else { // PM
+    if (!isTimeTwelve) {
+      return true;
+    }
+  }
+  return false;
+}
+
+
+
+
+
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function() {
   'use strict';
@@ -89,16 +141,16 @@ document.querySelector("#add-more").addEventListener("click", (event) => {
     const newFile = document.createElement("div");
     newFile.id = `file${filesCount}`;
     newFile.innerHTML = `
-    <div id="main-file-input${filesCount}" class="input-group mb-2 mr-sm-2">
-      <div onclick="fileInputActive(this.parentElement.parentElement)" id="browse${filesCount}" class="input-group-prepend hf">
-        <div class="input-group-text">Extra file</div>
+      <div id="main-file-input${filesCount}" class="input-group mb-2 mr-sm-2">
+        <div onclick="fileInputActive(this.parentElement.parentElement)" id="browse${filesCount}" class="input-group-prepend hf">
+          <div class="input-group-text">Extra file</div>
+        </div>
+        <input id="file-input${filesCount}" type="file" hidden>
+        <input onclick="fileInputActive(this.parentElement.parentElement)" id="fake-file-input${filesCount}" class="rounded-right form-control hf" placeholder="Click here" onkeydown="return false;" required >
+        <button onclick="removeFile(this)" type="button" class="close remove-file" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
       </div>
-      <input id="file-input${filesCount}" type="file" hidden>
-      <input onclick="fileInputActive(this.parentElement.parentElement)" id="fake-file-input${filesCount}" class="rounded-right form-control hf" placeholder="Click here" onkeydown="return false;" required >
-      <button onclick="removeFile(this)" type="button" class="close remove-file" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
     `;
     document.querySelector("#files-to-upload").appendChild(newFile);
     document.querySelector(`#fake-file-input${filesCount}`).click();
